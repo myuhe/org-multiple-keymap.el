8155e7c73254715acd4019e22ab632b63d1fe240
myuhe <yuhei.maeda@gmail.com>
2015-03-22 19:45:26 +0900

    Add test

--- test/org-multiple-keymap-test.el
+++ test/org-multiple-keymap-test.el
@@ -15,7 +15,7 @@
 * WAITING [#D] Test1   :@work: 
   SCHEDULED: <2015-03-22 日>
 
-** Subtree 
+** Subtree [#A]
     <2015-03-22 日>
   
 * TODO [#D] Test2      :@work:
@@ -24,17 +24,21 @@
 ")))
 
 (ert-deftest org-mukey-make-heading-alist-test ()
-  (setq expected '((139 . 140) (105 . 107) (44 . 45) (1 . 2)))
+  (setq expected '((143 . 144) (105 . 107) (44 . 45) (1 . 2)))
   (org-multiple-keymap-test-setup-buffer "*test*")
   (setq actual (org-mukey-make-heading-alist))
   (should (equal expected actual)))
 
 (ert-deftest org-mukey-make-timestamp-alist-test ()
   (setq expected (if (string= (substring (org-version) 0 3) "8.3")
-            '((89 . 103) (121 . 135) (184 . 202))
-            '((90 . 102) (122 . 134) (185 . 197))))
+                     '((89 . 103) (125 . 139) (188 . 206))
+                   '((90 . 102) (126 . 138) (189 . 201))))
     (org-multiple-keymap-test-setup-buffer "*test*")
     (setq actual (org-mukey-make-timestamp-alist))
     (should (equal expected actual)))
 
-
+(ert-deftest org-mukey-make-priority-alist-test ()
+  (setq expected '((54 . 58) (116 . 119) (150 . 154)))
+  (org-multiple-keymap-test-setup-buffer "*test*")
+  (setq actual (org-mukey-make-priority-alist))
+  (should (equal expected actual)))
